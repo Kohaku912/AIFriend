2fc86d8ba6c2709aa4814240f386849c030eda5e
Kohaku912 <kohaku99932@outlook.jp>
2025-09-13 12:02:47 +0900

    a

--- server/api/ruby.js
+++ server/api/ruby.js
@@ -1,6 +1,7 @@
 // api/ruby.js
 import fetch from 'node-fetch';
-
+import dotenv from 'dotenv';
+dotenv.config();
 export default async function handler(req, res) {
   // CORS対応
   res.setHeader('Access-Control-Allow-Origin', '*');
